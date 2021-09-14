0355e50c1f7d859572dc5d2151552dafa1b89445
BerkerBoyaci <berkerboyaci@gmail.com>
2021-09-14 09:34:33 +0300

    Update examples

--- examples/LogMacroToFileInChar.cpp
+++ examples/LogMacroToFileInChar.cpp
@@ -5,20 +5,18 @@ using namespace aricanli::general;
 
 int main() {
 
-	LOG_SET_FORMAT_C(" %m %t ")
-	LOG_SET_OUTPUT_C("basicLog\\log\\log.txt")
-	LOG_SET_OUTPUT_FORMAT_C()
+	LOG_SET_FORMAT_C(" %m %t ");
 
-	LOG_SET_PRIORITY_TRACE_C()
-	LOG_QUIET()
+	LOG_SET_PRIORITY_C(56);
+	LOG_QUIET();
 
-	LOG_FATAL_C(__LINE__, __FILE__, "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "Sed ornare commodo urna, eget sollicitudin ex eleifend ut.  ")
-	LOG_ERROR_C(__LINE__, __FILE__, "tellus felis condimentum odio, : ", 1, 'A', "porta tincidunt lectus turpis id sem. Integer et est neque.", "Vestibulum at maximus ante. ")
-	LOG_WARNING_C(__LINE__, __FILE__, "Nunc a varius purus. : ", 3.14159, "Proin dictum erat sit amet posuere tristique. ", "Fusce a arcu rhoncus, ")
-	LOG_INFO_C(__LINE__, __FILE__, "Nunc a varius purus. : ", 3.14159, "Proin dictum erat sit amet posuere tristique. ", "Fusce a arcu rhoncus, ")
-	LOG_VERBOSE_C(__LINE__, __FILE__, " mattis dolor id,  : ", " vestibulum nibh.", "Nulla in auctor leo. ")
-	LOG_DEBUG_C(__LINE__, __FILE__, "Nunc a varius purus. : ", 3.14159, "Proin dictum erat sit amet posuere tristique. ", "Fusce a arcu rhoncus, ")
-	LOG_TRACE_C(__LINE__, __FILE__, " mattis dolor id,  : ", 2.712, 'a', " vestibulum nibh.", "Nulla in auctor leo. ")
+	LOG_FATAL_C(__LINE__, __FILE__, "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "Sed ornare commodo urna, eget sollicitudin ex eleifend ut.  ");
+	LOG_ERROR_C(__LINE__, __FILE__, "tellus felis condimentum odio, : ", 1, 'A', "porta tincidunt lectus turpis id sem. Integer et est neque.", "Vestibulum at maximus ante. ");
+	LOG_WARNING_C(__LINE__, __FILE__, "Nunc a varius purus. : ", 3.14159, "Proin dictum erat sit amet posuere tristique. ", "Fusce a arcu rhoncus, ");
+	LOG_INFO_C(__LINE__, __FILE__, "Nunc a varius purus. : ", 3.14159, "Proin dictum erat sit amet posuere tristique. ", "Fusce a arcu rhoncus, ");
+	LOG_VERBOSE_C(__LINE__, __FILE__, " mattis dolor id,  : ", " vestibulum nibh.", "Nulla in auctor leo. ");
+	LOG_DEBUG_C(__LINE__, __FILE__, "Nunc a varius purus. : ", 3.14159, "Proin dictum erat sit amet posuere tristique. ", "Fusce a arcu rhoncus, ");
+	LOG_TRACE_C(__LINE__, __FILE__, " mattis dolor id,  : ", 2.712, 'a', " vestibulum nibh.", "Nulla in auctor leo. ");
 
 	return 0;
 }
--- examples/MacroToConsoleInWchar.cpp
+++ examples/MacroToConsoleInWchar.cpp
@@ -6,17 +6,16 @@ using namespace aricanli::general;
 
 int main() {
 
-	LOG_SET_FORMAT_W(L"%m %t")
-
-	LOG_SET_PRIORITY_VERBOSE_W()
-	LOG_QUIET()
-	LOG_FATAL_W(__LINE__, L"Lorem ipsum dolor sit amet, consectetur adipiscing elit.", L"Sed ornare commodo urna, eget sollicitudin ex eleifend ut.  ")
-	LOG_ERROR_W(__LINE__, L"tellus felis condimentum odio, : ", 1, 'A', L"porta tincidunt lectus turpis id sem. Integer et est neque.", L"Vestibulum at maximus ante. ")
-	LOG_WARNING_W(__LINE__, L"Nunc a varius purus. : ", 3.14159, L"Proin dictum erat sit amet posuere tristique. ", L"Fusce a arcu rhoncus, ")
-	LOG_INFO_W(__LINE__,  L"Nunc a varius purus. : ", 3.14159, L"Proin dictum erat sit amet posuere tristique. ", L"Fusce a arcu rhoncus, ")
-	LOG_VERBOSE_W(__LINE__, L" mattis dolor id,  : ", L" vestibulum nibh.", "Nulla in auctor leo. ")
-	LOG_DEBUG_W(__LINE__, L"Nunc a varius purus. : ", 3.14159, L"Proin dictum erat sit amet posuere tristique. ", L"Fusce a arcu rhoncus, ")
-	LOG_TRACE_W(__LINE__, L" mattis dolor id,  : ", 2.712, 'a', L" vestibulum nibh.", L"Nulla in auctor leo. ")
+	LOG_SET_FORMAT_W(L"%m %t");
+	LOG_SET_PRIORITY_W(48);
+	LOG_QUIET();
+	LOG_FATAL_W(__LINE__, L"Lorem ipsum dolor sit amet, consectetur adipiscing elit.", L"Sed ornare commodo urna, eget sollicitudin ex eleifend ut.  ");
+	LOG_ERROR_W(__LINE__, L"tellus felis condimentum odio, : ", 1, 'A', L"porta tincidunt lectus turpis id sem. Integer et est neque.", L"Vestibulum at maximus ante. ");
+	LOG_WARNING_W(__LINE__, L"Nunc a varius purus. : ", 3.14159, L"Proin dictum erat sit amet posuere tristique. ", L"Fusce a arcu rhoncus, ");
+	LOG_INFO_W(__LINE__, L"Nunc a varius purus. : ", 3.14159, L"Proin dictum erat sit amet posuere tristique. ", L"Fusce a arcu rhoncus, ");
+	LOG_VERBOSE_W(__LINE__, L" mattis dolor id,  : ", L" vestibulum nibh.", "Nulla in auctor leo. ");
+	LOG_DEBUG_W(__LINE__, L"Nunc a varius purus. : ", 3.14159, L"Proin dictum erat sit amet posuere tristique. ", L"Fusce a arcu rhoncus, ");
+	LOG_TRACE_W(__LINE__, L" mattis dolor id,  : ", 2.712, 'a', L" vestibulum nibh.", L"Nulla in auctor leo. ");
 
 	return 0;
 }
